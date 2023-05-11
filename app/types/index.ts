cd61c970c4c320bc9cf21e746c9691e721a18125
Zeeshan <zeeshanm2014@gmail.com>
2023-05-10 22:59:55 -0700

    types error fixed

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,28 +1,24 @@
+import { Listing, Reservation, User } from "@prisma/client";
 
-import { Reservation, User } from '@prisma/client'
-
-export type SafeListing = Omit<
-    Listing,
-    "createdAt"
-> & {
-    createdAt: string;
-}
+export type SafeListing = Omit<Listing, "createdAt"> & {
+  createdAt: string;
+};
 
 export type SafeReservation = Omit<
-    Reservation,
-    "createdAt" | "startDate" | "endDate" | "listing"
+  Reservation, 
+  "createdAt" | "startDate" | "endDate" | "listing"
 > & {
-    createdAt: string;
-    startDate: string;
-    endDate: string;
-    listing: SafeListing;
-}
+  createdAt: string;
+  startDate: string;
+  endDate: string;
+  listing: SafeListing;
+};
 
 export type SafeUser = Omit<
-    UserVerificationRequirement,
-    "createdAt" | "updatedAt" | "emailVerified"
+  User,
+  "createdAt" | "updatedAt" | "emailVerified"
 > & {
-    createdAt: string;
-    updatedAt: string;
-    emailVerified: string | null ;
-}
+  createdAt: string;
+  updatedAt: string;
+  emailVerified: string | null;
+};
